08bdabf21c61780c053b0e12dff5a129949e9f4c
Oceatoon <oceatoon@gmail.com>
2018-03-14 13:46:28 +0400

    small changes

--- views/default/index.php
+++ views/default/index.php
@@ -1,5 +1,6 @@
 <?php
 
+
 //assets from ph base repo
 $cssAnsScriptFilesTheme = array(
 	// SHOWDOWN
@@ -15,10 +16,14 @@ $cssAnsScriptFilesModule = array(
 	'/css/md.css',
 );
 HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule, Yii::app()->getModule( Yii::app()->params["module"]["parent"] )->getAssetsUrl() );
+
+if( $this->layout != "//layouts/empty"){
+	$layoutPath = 'webroot.themes.'.Yii::app()->theme->name.'.views.layouts.';
+	$this->renderPartial($layoutPath.'header',array("page"=>"ressource","layoutPath"=>$layoutPath));
+}
 ?>
 
-
-<h1 style="">
+<h1 style="margin-top: 50px; text-align: center;padding:10px;">
 	<img height=50 src="<?php echo Yii::app()->getModule( Yii::app()->params["module"]["parent"] )->getAssetsUrl()?>/images/CO.png">
 	<img height=50 src="<?php echo $this->module->assetsUrl?>/images/logo.png">
 	<?php echo CHtml::encode( (isset($this->module->pageTitle))?$this->module->pageTitle:""); ?>
@@ -29,10 +34,12 @@ HtmlHelper::registerCssAndScriptsFiles($cssAnsScriptFilesModule, Yii::app()->get
 <script type="text/javascript">
 
 $(document).ready(function() { 
-	getAjax('', baseUrl+'/'+moduleId+'/default/doc',
+	getAjax('', baseUrl+'/<?php  echo $this->module->id;?>/default/doc',
 		function(data){ 
+			alert();
 			descHtml = dataHelper.markdownToHtml(data); 
 			$('#doc').html(descHtml);
+			initKInterface();
 		},"html");
 });
 
